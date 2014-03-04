8bb7d6822a6d168b3d8c5573cb93cbd0eb674205
aitor.bv@deusto.es <aitor.bv@deusto.es@56bb20f6-f243-881f-c99c-54076dd6a040>
2014-03-04 15:54:59 +0000

    Ya se puede controlar el cliente desde la consola, poco a poco iré añadiendo mas métodos.

---  proyecto-p4-2013-2014-equipo-01/JeyTwitter/src/controller/GUIController.java
+++  proyecto-p4-2013-2014-equipo-01/JeyTwitter/src/controller/GUIController.java
@@ -1,8 +1,14 @@
 package controller;
 
+import java.awt.EventQueue;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import twitter4j.ResponseList;
 import twitter4j.Status;
 import twitter4j.TwitterException;
+import view.ventanas.Welcome;
 
 /**
  * Clase encargada de intermediar entre la GUI y la API de Twitter
@@ -16,9 +22,21 @@ public class GUIController {
 	
 	private TwitterService t;
 	private String token;
+	private Welcome welcomeWindow;
 	
 	public GUIController() {
 		t = new TwitterService(CONSUMER_KEY, CONSUMER_KEY_SECRET);
+		
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					//Welcome welcomeWindow = new Welcome();
+					//welcomeWindow.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 		
 		if (token == null) {
 			try {
@@ -52,26 +70,65 @@ public class GUIController {
 	/**
 	 * Tuitea usando el texto de el componente de la GUI correspondiente
 	 */
-	public void sendTweet() {
+	public void sendTweet(String texto) {
 		// Se supone que recoge el texto de el textfield de turno
 		try {
-			t.tweet("Probando JeyTuiter desde source code!!!");
+			t.tweet(texto);
 		} catch (TwitterException e) {
 			// Error al tuitear, mostrar mensaje
 			e.printStackTrace();
 		}
 	}
+	
+	public void menuConsole() {
+		String input = null;
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		do {
+			System.out.println("*****JeyTwitter V0.1*****");
+			System.out.println();
+			System.out.println();
+			System.out.println("1. Ver mi Timeline");
+			System.out.println("2. Ver mis menciones");
+			System.out.println("3. Ver tweets que he retwitteado");
+			System.out.println("4. Ver tweets que he marcado como favoritos");
+			System.out.println("5. Twittear un mensaje");
+			System.out.println("");
+			System.out.println("Pulsa 'q' para salir.");
+			System.out.println();
+			System.out.print("Seleciona una opción:");
+			try {
+				input = reader.readLine();
+				switch(input) {
+					case "1":showTimeline();
+					break;
+					
+					case "2":System.out.println("adios");
+					break;
+					
+					case "3":System.out.println("adios");
+					break;
+					
+					case "4":System.out.println("adios");
+					break;
+					
+					case "5":
+						System.out.print("Introduce el tweet: ");
+						String texto = reader.readLine();
+						sendTweet(texto);
+					break;
+				}
+				
+			} catch(IOException e) {
+				System.out.println("Error leyendo desde System.in");
+			    System.exit(1);
+			}
+		} while (!input.equals("q"));
+	}
 
 	public static void main(String[] args) {
 		// Inicio de el programa
 		GUIController gui = new GUIController();
-		
-		gui.showTimeline();
-		//gui.sendTweet();
-	}
-
-	public TwitterService getTwitterService() {
-		return t;
+		gui.menuConsole();
 	}
 
 }
