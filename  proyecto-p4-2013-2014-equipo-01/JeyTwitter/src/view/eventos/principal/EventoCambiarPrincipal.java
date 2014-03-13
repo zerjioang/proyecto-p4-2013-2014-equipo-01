2b9d691095681e4c92a8f206237dd7adc4af7f12
sergio.anguita@opendeusto.es <sergio.anguita@opendeusto.es@56bb20f6-f243-881f-c99c-54076dd6a040>
2014-03-13 21:24:42 +0000

    Se me ha olvidado añadir estas 2 clases.

---
+++  proyecto-p4-2013-2014-equipo-01/JeyTwitter/src/view/eventos/principal/EventoCambiarPrincipal.java
@@ -0,0 +1,50 @@
+package view.eventos.principal;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JTable;
+
+import view.elementos.botones.MenuButton;
+import view.ventanas.Principal;
+
+public class EventoCambiarPrincipal implements MouseListener {
+
+	private final JTable tablaGeneral;
+
+	public EventoCambiarPrincipal(Principal principal) {
+		tablaGeneral = principal.getTablaMenu();
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		int row = tablaGeneral.getSelectedRow();
+		int col = tablaGeneral.getSelectedColumn();
+		for (int i = 0; i < tablaGeneral.getRowCount(); i++) {
+			MenuButton b = (MenuButton) tablaGeneral.getValueAt(i, col);
+			if(i==row)
+				b.click();
+			else
+				b.exited();
+			tablaGeneral.setValueAt(b, i, col);
+		}
+		tablaGeneral.repaint();
+		
+		//Aqui viene el codigo que cambia de panel
+		
+		//Aqui viene el codigo que cambia de panel
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {}
+
+}
