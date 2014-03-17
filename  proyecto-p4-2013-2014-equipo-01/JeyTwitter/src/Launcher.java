e6fcbeb9f9e25c311fbfa3e8a6aea33cd6d6e5ee
aitor.bv@deusto.es <aitor.bv@deusto.es@56bb20f6-f243-881f-c99c-54076dd6a040>
2014-03-17 16:19:15 +0000

    Paquete Model traído de vuelta a la raíz de el repo

---  proyecto-p4-2013-2014-equipo-01/JeyTwitter/src/Launcher.java
+++  proyecto-p4-2013-2014-equipo-01/JeyTwitter/src/Launcher.java
@@ -16,6 +16,7 @@ public class Launcher {
 		Util.pausar(800);
 		spl.dispose();
 		System.out.println("Tenemos el pin? "+GUIController.getInstance().esTokenValido());
+		
 		if (GUIController.getInstance().esTokenValido()) {
 			// Tenemos token, lanzamos la ventana principal
 			Principal p = new Principal();
