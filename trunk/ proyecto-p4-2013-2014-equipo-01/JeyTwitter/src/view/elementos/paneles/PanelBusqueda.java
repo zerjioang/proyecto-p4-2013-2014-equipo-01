cb409dbaa2f43de58217b50dccd7d82747c2a305
sergio.anguita@opendeusto.es <sergio.anguita@opendeusto.es@56bb20f6-f243-881f-c99c-54076dd6a040>
2014-04-06 14:54:11 +0000

    He puesto un par de tweets de prueba en la tabla de los resultados de busqueda

--- trunk/ proyecto-p4-2013-2014-equipo-01/JeyTwitter/src/view/elementos/paneles/PanelBusqueda.java
+++ trunk/ proyecto-p4-2013-2014-equipo-01/JeyTwitter/src/view/elementos/paneles/PanelBusqueda.java
@@ -68,7 +68,7 @@ public class PanelBusqueda extends JPanel {
 		JScrollPane scrollPaneTablaBusqueda = new JScrollPane();
 		panelBusquedaTabla.add(scrollPaneTablaBusqueda, BorderLayout.CENTER);
 		
-		tablaBusqueda = new TablaTweetsUsuarios(TablaTweetsUsuarios.SOLO_TWEETS);
+		tablaBusqueda = new TablaTweetsUsuarios(objeto);
 		scrollPaneTablaBusqueda.setViewportView(tablaBusqueda);
 	}
 
