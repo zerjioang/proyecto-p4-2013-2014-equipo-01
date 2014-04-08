0e4cfcaf5b7aef97d7ab8c914a7799576474330f
sergio.anguita@opendeusto.es <sergio.anguita@opendeusto.es@56bb20f6-f243-881f-c99c-54076dd6a040>
2014-04-08 08:47:22 +0000

    Editados contadores

--- trunk/ proyecto-p4-2013-2014-equipo-01/JeyTwitter/src/view/eventos/principal/EventoVerContadorPerfilusuario.java
+++ trunk/ proyecto-p4-2013-2014-equipo-01/JeyTwitter/src/view/eventos/principal/EventoVerContadorPerfilusuario.java
@@ -6,15 +6,22 @@ import java.awt.event.MouseMotionListener;
 
 import view.elementos.paneles.PanelPerfilUsuario;
 import view.ventanas.Contador;
+import view.ventanas.Principal;
 
 public class EventoVerContadorPerfilusuario implements MouseListener, MouseMotionListener {
 
+	public static final int FAVORITOS = 0;
+	public static final int TWEETS = 1;
+	public static final int SEGUIDORES = 2;
+	public static final int SIGUIENDO = 3;
 	private final PanelPerfilUsuario p;
-	private static Contador c;
+	private Contador c;
+	private int opcion;
 
-	public EventoVerContadorPerfilusuario(PanelPerfilUsuario panelPerfilUsuario, Contador c) {
+	public EventoVerContadorPerfilusuario(PanelPerfilUsuario panelPerfilUsuario, Contador c, int opcion) {
 		p = panelPerfilUsuario;
 		this.c = c;
+		this.opcion = opcion;
 	}
 
 	@Override
@@ -25,7 +32,20 @@ public class EventoVerContadorPerfilusuario implements MouseListener, MouseMotio
 
 	@Override
 	public void mouseEntered(MouseEvent e) {
-		c.setCantidad(500);
+		switch (opcion) {
+		case 0:
+			c.setCantidad(Principal.getUsuarioActual().getNumeroTweets());
+			break;
+		case 1:
+			c.setCantidad(Principal.getUsuarioActual().getNumeroTweets());
+			break;
+		case 2:
+			c.setCantidad(Principal.getUsuarioActual().getNumeroSeguidores());
+			break;
+		case 3:
+			c.setCantidad(Principal.getUsuarioActual().getNumeroSiguiendo());
+			break;
+		}
 		mover(e);
 		c.setVisible(true);
 	}
