0f98830e4ef4cc17f24d28e872e08892e8cf0979
sergiers@opendeusto.es <sergiers@opendeusto.es@56bb20f6-f243-881f-c99c-54076dd6a040>
2014-04-10 18:39:52 +0000

    Agregado el metodo que hace la grafica. Falta por mejorarlo un poco

--- trunk/ proyecto-p4-2013-2014-equipo-01/JeyTwitter/src/monitorizacion/Grafica.java
+++ trunk/ proyecto-p4-2013-2014-equipo-01/JeyTwitter/src/monitorizacion/Grafica.java
@@ -1,99 +1,124 @@
-/**
- * Clase encargada de dibujar una grafica simple. Esta clase crea una imagen JPG con la gr��fica
- * 
- * PD: perdon por el uso de m��todos deprecados :(
- */
-
-
 package monitorizacion;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.file.attribute.UserPrincipalNotFoundException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartUtilities;
 import org.jfree.chart.JFreeChart;
+import org.jfree.data.time.Day;
+import org.jfree.data.time.TimeSeriesCollection;
 
-import java.io.*;
-import java.util.Vector;
+import twitter4j.*;
 
-import org.jfree.data.time.*;
 
 public class Grafica {
-	
-	
-	/**
-	 * Metodo que crea la grafica. Necesita un vector de "Datos"
-	 * 
-	 * @author sergio
-	 * @param vector
-	 */
-	public static void crearGrafica(Vector<Dato> vector) {
-		
-	org.jfree.data.time.TimeSeries pop = new org.jfree.data.time.TimeSeries("Linea de Crecimiento", Day.class);
-	
-	/*
-	 * El tema es este: no puedes a��adir 1-1-14 si ya hay un 1-1-14. Asi que hay que usar el metodo uddOrUpdate()
-	 * sin embargo, este m��todo sobreescribe.
-	 * 
-	 * Para que no den datos raros, tiene que haber al menos 2 fechas
-	 */
-	
 
+	@SuppressWarnings("deprecation")
+	public static void crearGrafica(String usuario, Twitter t) throws TwitterException, FileNotFoundException, ParseException {
 
-	for(int i=0; i<vector.size() ; i++){
 		
-		Dato datos = vector.elementAt(i);
+		//Abrimos un txt
+		//PrintStream DDescritor = new PrintStream("tuits.txt");
+
+		//iniciamos la grafica
+		org.jfree.data.time.TimeSeries pop = new org.jfree.data.time.TimeSeries("Historial de tuits", Day.class);
+
+		//calculo: obtener cuantas paginas hay
+		ResponseList<Status> timeline = t.getUserTimeline(usuario, new Paging(1));
+		int numT = timeline.get(1).getUser().getStatusesCount();
 		
-		int dia = datos.dia;
-		int mes = datos.mes;
-		int ano = datos.ano;
+		int div = (int) (numT/200) + 1;
+
 		
-		pop.addOrUpdate(new Day(dia, mes, ano), 1);			
-		//pop.add(new Day(dia, mes, ano), 1);
+		
+		int contador =1;
+		for(int j=1; j<=div; j++){
+		ResponseList<Status> tuits = t.getUserTimeline(usuario, new Paging(j, 200));
+		
+		for(int i=0; i<tuits.size()-1;i++){
+			//por cada tuí
+			System.out.println(tuits.get(i).getText());
+			Date primera = new Date(tuits.get(i).getCreatedAt().getYear() + 1900, tuits.get(i).getCreatedAt().getMonth(), tuits.get(i).getCreatedAt().getDate());
+			Date segunda = new Date(tuits.get(i+1).getCreatedAt().getYear() + 1900, tuits.get(i+1).getCreatedAt().getMonth(), tuits.get(i+1).getCreatedAt().getDate());
+
+			if(segunda.equals(primera)){
+				contador++;				
+			}else{
+				//DDescritor.println((primera.getDate())+"-"+(1 + primera.getMonth())+"-"+primera.getYear()+ ": " + contador);
 				
-	}
-	
-	pop.add(new Day(1, 4, 114), 1);			
-	pop.add(new Day(2, 4, 114), 2);			
+				pop.add(new Day(primera.getDate(), primera.getMonth()+1, primera.getYear()), contador);
+				contador = 1;
 
-
-	
-	String nom = vector.firstElement().getNom();
-
-	
-	
-	TimeSeriesCollection dataset = new TimeSeriesCollection();
-	dataset.addSeries(pop);
-	JFreeChart chart = ChartFactory.createTimeSeriesChart("Historial de tuits de "+ nom,"Fecha","Numero de Tuits",dataset,true,true,false);
-	
-	try {
-		ChartUtilities.saveChartAsJPEG(new File("grafica.jpg"), chart, 500, 300);
-		}catch (IOException e){
-			System.err.println("Error creando grafico.");
 			}
+			
+			
+
+			
+			
 		}
-	
-	
-	
-	
-	public static void main(String[] args){
-
-		org.jfree.data.time.TimeSeries pop = new org.jfree.data.time.TimeSeries("Linea de Crecimiento", Day.class);
 		
-		
-
-		pop.addOrUpdate(new Day(1, 2, 2014), 1);
-		pop.addOrUpdate(new Day(2, 2, 2014), 2);
-		pop.addOrUpdate(new Day(3, 2, 2014), 5);
-		pop.addOrUpdate(new Day(4, 2, 2014), 2);
-
-
+		}
+					
+					
+				
+		//cerramos la grafica y el fitxero
+		//DDescritor.close();
 		TimeSeriesCollection dataset = new TimeSeriesCollection();
 		dataset.addSeries(pop);
-		JFreeChart chart = ChartFactory.createTimeSeriesChart("Historial de tuits de PRUEBAAAA","Fecha","Numero de Tuits",dataset,true,true,false);
-		
+		JFreeChart chart = ChartFactory.createTimeSeriesChart("Historial de tuits de: " + usuario,"Fecha","Numero de Tuits",dataset,true,true,false);
+
 		try {
-			ChartUtilities.saveChartAsJPEG(new File("rafica.png"), chart, 2000, 1200);
-			}catch (IOException e){
-				System.err.println("Error creando grafico.");
-				}
+			ChartUtilities.saveChartAsPNG(new File("grafica_de_@"+usuario+".jpg"), chart, 2000, 1200);
+		}catch (IOException e){
+			System.err.println("Error creando grafico.");
+		}
 	}
-}
+
+
+
+
+	/*esto da tuits infinitos :3
 	
+	Query query = new Query("#live");
+	query.setCount(100);
+	QueryResult result=t.search(query);
+	do{
+	          List<Status> tweets = result.getTweets();
+	          for(Status tweet: tweets){
+	               System.out.println("Tweet: "+tweet.getText());
+	          }
+	          query=result.nextQuery();
+	          if(query!=null)
+	               result=t.search(query);
+	}while(query!=null);
+	
+	
+	*/
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
