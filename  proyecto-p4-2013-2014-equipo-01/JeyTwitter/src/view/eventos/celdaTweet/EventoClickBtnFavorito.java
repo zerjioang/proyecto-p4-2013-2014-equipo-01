975f593c4ee3b634cbd62f6aeee16f03e2ec082d
ruben.garcia@opendeusto.es <ruben.garcia@opendeusto.es@56bb20f6-f243-881f-c99c-54076dd6a040>
2014-05-05 14:23:42 +0000

    Commit para anguita

---  proyecto-p4-2013-2014-equipo-01/JeyTwitter/src/view/eventos/celdaTweet/EventoClickBtnFavorito.java
+++  proyecto-p4-2013-2014-equipo-01/JeyTwitter/src/view/eventos/celdaTweet/EventoClickBtnFavorito.java
@@ -3,42 +3,40 @@ package view.eventos.celdaTweet;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
+import controller.GUIController;
+import twitter4j.TwitterException;
 import view.elementos.GUITweet;
 
 public class EventoClickBtnFavorito implements MouseListener {
 
+	private final GUITweet t;
+	
 	public EventoClickBtnFavorito(GUITweet guiTweet) {
-		// TODO Auto-generated constructor stub
+		t = guiTweet;
 	}
 
 	@Override
-	public void mouseClicked(MouseEvent e) {
+	public void mouseClicked(MouseEvent event) {
 		// TODO Auto-generated method stub
-		//NECESITO AQUÍ EL LONG DEL TWEET
-		//favorito(codigoTweet);
+		try {
+			long codigo = t.getTweet().getCodigo();
+			GUIController.getInstance().getT().favorito(codigo);
+		} catch (TwitterException e) {
+			System.err.println("Ha ocurrido un error al procesar el tweet para favorito");
+			System.err.println("Detalles: "+e.getMessage());
+		}
 	}
 
 	@Override
-	public void mouseEntered(MouseEvent e) {
-		// TODO Auto-generated method stub
-
-	}
+	public void mouseEntered(MouseEvent e) {}
 
 	@Override
-	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
-
-	}
+	public void mouseExited(MouseEvent e) {}
 
 	@Override
-	public void mousePressed(MouseEvent e) {
-		// TODO Auto-generated method stub
-	}
+	public void mousePressed(MouseEvent e) {}
 
 	@Override
-	public void mouseReleased(MouseEvent e) {
-		// TODO Auto-generated method stub
-
-	}
+	public void mouseReleased(MouseEvent e) {}
 
 }
