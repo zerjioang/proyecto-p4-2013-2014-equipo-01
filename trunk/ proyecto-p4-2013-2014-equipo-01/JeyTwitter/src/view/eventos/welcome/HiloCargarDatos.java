c4f552bb3eb7d65e838406c152cdc0f099276621
sergio.anguita@opendeusto.es <sergio.anguita@opendeusto.es@56bb20f6-f243-881f-c99c-54076dd6a040>
2014-05-06 15:30:45 +0000

    Se me olvidaba esta clase

---
+++ trunk/ proyecto-p4-2013-2014-equipo-01/JeyTwitter/src/view/eventos/welcome/HiloCargarDatos.java
@@ -0,0 +1,41 @@
+package view.eventos.welcome;
+
+import javax.swing.JFrame;
+
+import util.InvalidInputException;
+import util.Util;
+import view.systray.SystemTrayLogo;
+import view.ventanas.Bienvenida;
+import view.ventanas.Principal;
+import controller.GUIController;
+
+public class HiloCargarDatos extends Thread{
+
+	private final Bienvenida ventana;
+	
+	public HiloCargarDatos(Bienvenida ventana) {
+		this.ventana = ventana;
+	}
+
+	public void run(){
+		if(ventana.getCodeField().evaluate()){
+			try {
+				/*Principal p = new Principal(GUIController.getInstance().getUsuarioRegistrado());
+				GUIController.getInstance().guardarUsuario(ventana.getCodigo());
+				p.setLocationRelativeTo(ventana);
+				p.setPanelActual(p.getPaneles()[1]);
+				p.setVisible(false);*/
+				//la ventana se minimiza y aparece un globo emergente
+				Util.showMessage(ventana, "Autentificacion OAuth", "Usuario autentificado correctamente", "Continuar", "Cancelar");
+				ventana.cerrar();
+				//aparece el globo emergente
+				SystemTrayLogo l = SystemTrayLogo.getInstace();
+				l.enviarMensaje("Descargando datos", Util.APP_TITULO+" esta descargando los datos mas recientes");
+				l.setTitulo("descargando datos");
+			} catch (IllegalStateException | InvalidInputException e) {
+				ventana.setVisible(true);
+				Util.showError(ventana, "Error de autentificacion", "El token no es valido", "Cancelar", "Aceptar");
+			}
+		}
+	}
+}
