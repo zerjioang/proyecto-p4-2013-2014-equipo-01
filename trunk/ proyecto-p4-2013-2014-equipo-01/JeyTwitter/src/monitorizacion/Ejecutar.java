f098947c792ec40584323a1f4fcd33c7e20ca168
sergiers@opendeusto.es <sergiers@opendeusto.es@56bb20f6-f243-881f-c99c-54076dd6a040>
2014-05-16 15:12:36 +0000

    Conflictos

---
+++ trunk/ proyecto-p4-2013-2014-equipo-01/JeyTwitter/src/monitorizacion/Ejecutar.java
@@ -0,0 +1,39 @@
+package monitorizacion;
+
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class Ejecutar {
+
+	/*
+	 * 					inicio
+	 * 						|<---------------------------
+	 * 						|							|
+	 * 				¿Hay archivo?--no--generar archivo-->
+	 * 						|  					  ^	
+	 * 						si					  |
+	 * 						|					  |
+	 * 			¿hay que generarlo otra vez?--si--
+	 * 						|
+	 * 						|
+	 * 					fin de esta cosa
+	 */
+	
+	
+	public void empezarFlujo(){
+		
+		try{
+			FileInputStream fis = new FileInputStream("timelineUsuario.jey"); 
+			ObjectInputStream ois = new ObjectInputStream(fis);
+			
+			
+		}catch(Exception e){
+			
+			
+		}
+		
+	}
+	
+	
+}
