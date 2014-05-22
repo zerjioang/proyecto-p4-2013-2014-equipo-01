fb823fc6a0c96b2e1fc1bbede0a35bf04dc9ac29
sergio.anguita@opendeusto.es <sergio.anguita@opendeusto.es@56bb20f6-f243-881f-c99c-54076dd6a040>
2014-05-22 20:38:44 +0000

    Evento del boton estadistica añadido

---
+++ trunk/ proyecto-p4-2013-2014-equipo-01/JeyTwitter/src/view/eventos/estadistica/EventoClickIniciar.java
@@ -0,0 +1,25 @@
+package view.eventos.estadistica;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class EventoClickIniciar implements MouseListener {
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		//Aqui viene el codigo a ejecutar al clicar en iniciar
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {}
+
+	@Override
+	public void mouseExited(MouseEvent e) {}
+
+}
